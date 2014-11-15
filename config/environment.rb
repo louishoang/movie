862c3dc6b002ddd122152e8704a0033140c0183b
louis hoang <bawsing@gmail.com>
2014-11-14 23:05:21 -0500

    format date and time in environment.rb for whole app

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Time::DATE_FORMATS[:format_date_time] = "%B %d, %Y at %I:%M %p"
+Time::DATE_FORMATS[:format_date] = "%B %d, %Y"
