63e78ac44d53ffbaf6734e2bee1325df0a989c00
louis hoang <bawsing@gmail.com>
2014-11-14 23:23:18 -0500

    admin can delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,5 +10,12 @@ module Admin
         @users = User.order(:email).page params[:page]
       end
     end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      flash[:notice] = "User has been successfully deleted"
+      redirect_to admin_users_path
+    end
   end
 end
